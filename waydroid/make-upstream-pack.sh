22f8ee70a9d5dc9815143c53836eb0cf17f36ff4
Basil Stotz <stotz.basil@opinsys.fi>
2023-06-08 16:46:08 +0200

    update

--- waydroid/make-upstream-pack.sh
+++ waydroid/make-upstream-pack.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+
+if test -z "$(which waydroid)"; then
+    echo "error: waydroid not installed"
+    exit 1
+fi
+
 tempdir=$(mktemp -d /tmp/waydroid.XXXXXX)
 
 mkdir -p /var/lib/waydroid
 mount --bind "$tempdir" /var/lib/waydroid
+
 waydroid init
 
 tar --mtime='2000-01-01 00:00:00 +00:00' \
